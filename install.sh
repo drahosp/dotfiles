87079f001bca61692542623e855b01c66ea11862
Peter Drahoš <pd@PDR.local>
2015-11-26 12:11:16 +0100

    Fix neovim config install destination

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@ set -e
 # Clean up targets
 rm -rf $HOME/.config/nvim* $HOME/.zsh* $HOME/.tmux*
 
+# Make subdirs
+mkdir -p $HOME/.config/nvim
+
 # Symlink files
 ln -s $PWD/config/zsh $HOME/.zshrc
 ln -s $PWD/config/tmux $HOME/.tmux.conf
-ln -s $PWD/config/neovim $HOME/.nvimrc
+ln -s $PWD/config/neovim $HOME/.config/nvim/init.vim
 
